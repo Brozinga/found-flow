8c10bdbb31a1f13ff9ab5fee51d26b2652b632b3
Fernando Brozinga <fbrozinga@outlook.com>
2024-04-29 21:24:36 -0300

    feat: Habilitado a autorização no graphql.

--- backend/src/FoundFlow.Infrastructure/Database/Queries/Query.cs
+++ backend/src/FoundFlow.Infrastructure/Database/Queries/Query.cs
@@ -2,22 +2,26 @@
 using FoundFlow.Domain.Entities;
 using FoundFlow.Infrastructure.Database.SortTypes;
 using HotChocolate;
+using HotChocolate.Authorization;
 using HotChocolate.Data;
 
 namespace FoundFlow.Infrastructure.Database.Queries;
 
 public class Query
 {
+    [Authorize]
     [UseProjection]
     [UseFiltering]
     [UseSorting(typeof(CategoriesSortInputType))]
     public IQueryable<Categories> GetCategories([Service] ApplicationDbContext context) => context.Categories;
 
+    [Authorize]
     [UseProjection]
     [UseFiltering]
     [UseSorting(typeof(TransactionsSortInputType))]
     public IQueryable<Transactions> GetTransactions([Service] ApplicationDbContext context) => context.Transactions;
 
+    [Authorize]
     [UseProjection]
     [UseFiltering]
     [UseSorting(typeof(UsersSortInputType))]
