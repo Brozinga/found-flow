41cf80aca281eb0ee756de0b599b04d21805016f
Fernando Brozinga <fbrozinga@outlook.com>
2024-08-30 01:29:48 -0300

    feat: iniciando front-end.

--- frontend/FoundFlow.Frontend/src/FoundFlow.Frontend.Base/Program.cs
+++ frontend/FoundFlow.Frontend/src/FoundFlow.Frontend.Base/Program.cs
@@ -6,8 +6,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveWebAssemblyComponents();
 
-builder.Services.AddBlazorBootstrap();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -18,7 +16,6 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
