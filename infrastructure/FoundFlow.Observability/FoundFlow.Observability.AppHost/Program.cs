bff0c24f235e122fafe0a4b4333b6ba4083d1803
Fernando Brozinga <fbrozinga@outlook.com>
2024-09-01 12:21:21 -0300

    feat: removido o Redis.

--- infrastructure/FoundFlow.Observability/FoundFlow.Observability.AppHost/Program.cs
+++ infrastructure/FoundFlow.Observability/FoundFlow.Observability.AppHost/Program.cs
@@ -1,7 +1,5 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var cache = builder.AddRedis("cache");
-
 var webapi = builder.AddProject<Projects.FoundFlow_WebApi>("foundflow-webapi");
 
 builder.AddProject<Projects.FoundFlow_Frontend_Base>("foundflow-frontend")
