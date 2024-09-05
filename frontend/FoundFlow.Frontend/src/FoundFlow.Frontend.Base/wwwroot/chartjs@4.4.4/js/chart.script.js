2dcfe651bfc7cc60d95bda5d552e25100344c204
Fernando Brozinga <fbrozinga@outlook.com>
2024-09-05 01:00:40 -0300

    feat: Adicionado gráficos para análise de receitas e despesas.

---
+++ frontend/FoundFlow.Frontend/src/FoundFlow.Frontend.Base/wwwroot/chartjs@4.4.4/js/chart.script.js
@@ -0,0 +1,56 @@
+﻿window.RunningDashChart = (canvasId, labels, data) => {
+    const ctx = document.getElementById(canvasId);
+
+    new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: '',
+                data: data,
+                borderColor: '#4e7fe8',
+                tension: 0.1,
+                formatter: (value) => {
+                    return value.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+                }
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true,
+                    ticks: {
+                        callback: (value) => {
+                            return value.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+                        }
+                    }
+                }
+            },
+            elements: {
+              point: {
+                  radius: 6,
+                  backgroundColor: '#4e7fe8'
+              }  
+            },
+            plugins: {
+                legend: {
+                    display: false
+                },
+                tooltip: {
+                    callbacks: {
+                        label: (context) => {
+                            let label = context.dataset.label || '';
+                            if (label) {
+                                label += ': ';
+                            }
+                            if (context.parsed.y !== null) {
+                                label += context.parsed.y.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+                            }
+                            return label;
+                        }
+                    }
+                }
+            }
+        }
+    });
+}
